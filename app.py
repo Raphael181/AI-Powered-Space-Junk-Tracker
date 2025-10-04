c17697e2fb0e75052c1bf660f0c0770879945f1b
Raphael <madur0392@gmail.com>
2025-10-04 15:33:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,6 @@ st.markdown("""
     .stButton button {
         border: none;
         color: white;
-        border-radius: 0.8px;
         padding: 0.6rem 1rem;
     }
     .stButton button:hover {
